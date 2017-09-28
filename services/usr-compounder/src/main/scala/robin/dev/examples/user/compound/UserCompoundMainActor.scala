247d20249351f17627b8b5887e78507b3c4f0e06
Tuan.vu(NT) <tuan.vu@niteco.se>
2017-09-28 14:11:32 +0700

    fix missing implicit parameter

--- services/usr-compounder/src/main/scala/robin/dev/examples/user/compound/UserCompoundMainActor.scala
+++ services/usr-compounder/src/main/scala/robin/dev/examples/user/compound/UserCompoundMainActor.scala
@@ -1,5 +1,6 @@
 package robin.dev.examples.user.compound
 
+import akka.actor.ActorSystem
 import akka.actor.{Actor, Props}
 import org.slf4j.LoggerFactory
 import robin.dev.examples.common.Configs
@@ -8,6 +9,7 @@ import robin.dev.examples.common.Configs
 class UserCompoundMainActor(port: Int) extends Actor{
 
   private implicit val log = LoggerFactory.getLogger(getClass)
+  implicit val system: ActorSystem = context.system
 
   def receive: Receive = {
     case m: RabbitMq.Out.OutEvent  => storageActor forward m.e
